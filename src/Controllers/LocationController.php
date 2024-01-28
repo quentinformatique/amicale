bf3d492284eda49547c306a43ef5de3b6b05b5ea
quentinformatique <quentin161204@gmail.com>
2024-01-28 20:24:45 +0100

    add controllers

---
+++ src/Controllers/LocationController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace MvcLite\Controllers;
+
+use MvcLite\Controllers\Engine\Controller;
+use MvcLite\Views\Engine\View;
+
+class LocationController extends Engine\Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        // Empty constructor.
+    }
+
+    public function render(): void
+    {
+        View::render("location");
+    }
+
+}
---
+++ src/Controllers/SellController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MvcLite\Controllers;
+
+use MvcLite\Controllers\Engine\Controller;
+use MvcLite\Views\Engine\View;
+
+class SellController extends Engine\Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        // Empty constructor.
+    }
+
+    public function render(): void
+    {
+        View::render("sell");
+    }
+}
