b962c8a9d0fe93b0c327b0c91f3ee1705017260e
quentinformatique <quentin161204@gmail.com>
2024-01-28 20:26:16 +0100

    change views to twig

--- src/Router/routes.php
+++ src/Router/routes.php
@@ -6,10 +6,17 @@
  */
 
 
+use MvcLite\Controllers\BarterController;
 use MvcLite\Controllers\IndexController;
+use MvcLite\Controllers\LocationController;
 use MvcLite\Controllers\MessagesController;
+use MvcLite\Controllers\PublishController;
+use MvcLite\Controllers\SellController;
 use MvcLite\Router\Engine\Router;
 
-// Create your own routes!
 
 Router::get("/", IndexController::class, "render")->setName("index");
+Router::get("/Publish", PublishController::class, "render")->setName("publish");
+Router::get("/Location", LocationController::class, "render")->setName("location");
+Router::get("/Barter", BarterController::class, "render")->setName("barter");
+Router::get("/Sell", SellController::class, "render")->setName("sell");
