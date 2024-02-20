dada8d95f0756a5085618ed8f5e57ab11336a0f3
quentinformatique <quentin161204@gmail.com>
2024-02-20 23:52:00 +0100

    made location backend

--- src/Controllers/LocationController.php
+++ src/Controllers/LocationController.php
@@ -3,13 +3,25 @@
 namespace MvcLite\Controllers;
 
 use MvcLite\Controllers\Engine\Controller;
+use MvcLite\Models\Offer;
 use MvcLite\Views\Engine\View;
 
 class LocationController extends Engine\Controller
 {
     public function render(): void
     {
-        View::render("Location");
+        $sortOrder = $_GET['filter'] ?? 'date'; // Get the sort order from the request
+        View::render("Sell", [
+            "offers" => $this->getVerifiedLocationsOffers($sortOrder)
+        ]);
+    }
+
+    private function getVerifiedLocationsOffers(mixed $sortOrder): array
+    {
+        return match ($sortOrder) {
+            'price' => Offer::getVerifiedOffersByCategorySortedByPrice("1"),
+            default => Offer::getVerifiedOffersByCategorySortedByDate("1"),
+        };
     }
 
 }
