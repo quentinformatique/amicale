61de34ce5a3968847391920a9652b50473a8d99d
quentinformatique <quentin161204@gmail.com>
2024-02-20 23:49:28 +0100

    created barter controller

--- src/Controllers/BarterController.php
+++ src/Controllers/BarterController.php
@@ -3,6 +3,7 @@
 namespace MvcLite\Controllers;
 
 use MvcLite\Controllers\Engine\Controller;
+use MvcLite\Models\Offer;
 use MvcLite\Views\Engine\View;
 
 class BarterController extends Engine\Controller
@@ -11,7 +12,18 @@ class BarterController extends Engine\Controller
 
     public function render(): void
     {
-        View::render("Barter");
+        $sortOrder = $_GET['filter'] ?? 'date'; // Get the sort order from the request
+        View::render("Barter", [
+            "offers" => $this->getVerifiedBarterOffers($sortOrder)
+        ]);
+    }
+
+    private function getVerifiedBarterOffers(mixed $sortOrder): array
+    {
+        return match ($sortOrder) {
+            'price' => Offer::getVerifiedOffersByCategorySortedByPrice("3"),
+            default => Offer::getVerifiedOffersByCategorySortedByDate("3"),
+        };
     }
 
 }
