2ec6dd3ab9968943591e2751d6be30f05d9d3d33
quentinformatique <quentin161204@gmail.com>
2024-02-26 21:23:19 +0100

    Update User.php

--- src/Models/User.php
+++ src/Models/User.php
@@ -2,26 +2,38 @@
 
 namespace MvcLite\Models;
 
-use MvcLite\Database\Engine\Database;
-use MvcLite\Models\Engine\Model;
+use MvcliteCore\Models\Model;
 
 class User extends Model
 {
 
 
-    public static function newAgent(mixed $agentCode, mixed $phone, mixed $email): \MvcLite\Database\Engine\DatabaseQuery
+    public static function newAgent(mixed $agentCode, mixed $phone, mixed $email): Model
     {
-        return Database::query("INSERT INTO users (code_agent, phone, mail) VALUES (?, ?, ?)", [$agentCode, $phone, $email]);
+        return User::create([
+            'code_agent' => $agentCode,
+            'phone' => $phone,
+            'mail' => $email
+        ]);
     }
 
-    public static function getUserByCode(mixed $agentCode): Engine\ModelCollection
+    public static function getUserByCode(mixed $agentCode): \MvcliteCore\Models\ModelCollection
     {
         return User::select()
-            ->where('code_agent', '=', $agentCode)
+            ->where('code_agent',  $agentCode)
             ->execute();
     }
 
-    public function getUserById(int $publisherId): Engine\ModelCollection
+    public static function getUser(string $agentCode, string $phone, string $email): \MvcliteCore\Models\ModelCollection
+    {
+        return User::select('id')
+            ->where('code_agent',  $agentCode)
+            ->where('phone',  $phone)
+            ->where('mail',  $email)
+            ->execute();
+    }
+
+    public function getUserById(int $publisherId): \MvcliteCore\Models\ModelCollection
     {
 
         return User::select('users')
