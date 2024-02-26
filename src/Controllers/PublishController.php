b34a352d4bfee7989f7244f5564e03321317eb52
quentinformatique <quentin161204@gmail.com>
2024-02-26 21:21:54 +0100

    Update PublishController.php

--- src/Controllers/PublishController.php
+++ src/Controllers/PublishController.php
@@ -2,13 +2,12 @@
 
 namespace MvcLite\Controllers;
 
-use MvcLite\Controllers\Engine\Controller;
-use MvcLite\Engine\DevelopmentUtilities\Debug;
+use MvcliteCore\Controllers\Controller;
 use MvcLite\Models\Offer;
-use MvcLite\Router\Engine\Request;
-use MvcLite\Views\Engine\View;
+use MvcliteCore\Router\Request;
+use MvcliteCore\Views\View;
 
-class PublishController extends Engine\Controller
+class PublishController extends Controller
 {
 
     public function render(): void
@@ -29,6 +28,16 @@ class PublishController extends Engine\Controller
         $accept = $request->getInput('accept');
         $type = $request->getInput('type');
 
+
+
+
+        // Validate form data
+        if (empty($title) || empty($price) || empty($description) || empty($agentCode) || empty($phone) || empty($email) || $accept != "on" || empty($type)) {
+            // Return error message to user
+            View::render("Publish", ['error' => 'Tous les champs sont obligatoires.']);
+            return;
+        }
+
         // type gestion
         if (sizeof($type) > 1) {
             View::render("Publish", ['error' => 'Veuillez choisir un seul type.']);
@@ -41,30 +50,15 @@ class PublishController extends Engine\Controller
         }
 
 
-        // Validate form data
-        if (empty($title) || empty($price) || empty($description) || empty($agentCode) || empty($phone) || empty($email) || $accept != "on" || empty($type)) {
-            // Return error message to user
-            Debug::dd($title, $price, $description, $agentCode, $phone, $email, $accept, $type);
-            View::render("Publish", ['error' => 'Tous les champs sont obligatoires.']);
-            return;
+        // Handle file upload
+        if (isset($_FILES['photo']) && $_FILES['photo']['error'] === UPLOAD_ERR_OK) {
+            $photo = $_FILES['photo'];
+            $photoPath = "uploads/" . $photo['name'];
+            move_uploaded_file($photo['tmp_name'], $photoPath);
+        } else {
+            View::render("Publish", ['error' => 'Veuillez choisir une photo.']);
         }
 
-        // Handle file uploads
-        /* TODO: Handle file uploads
-        $photoPaths = [];
-        for ($i = 0; $i < count($photos['name']); $i++) {
-            if (is_uploaded_file($photos['tmp_name'][$i])) {
-                $targetPath = "Resources/Medias/databaseImages/" . basename($photos['name'][$i]);
-                if (move_uploaded_file($photos['tmp_name'][$i], $targetPath)) {
-                    $photoPaths[] = $targetPath;
-                } else {
-                    // Return error message to user
-                    View::render("Publish", ['error' => 'Failed to upload photo.']);
-                    return;
-                }
-            }
-        }
-        */
 
         // Save Offer to database
         $photoPath= "empty";
