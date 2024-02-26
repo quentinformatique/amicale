be42be77595fad3b772f488721ab3f53f8764e7d
quentinformatique <quentin161204@gmail.com>
2024-02-26 21:21:37 +0100

    Update BarterController.php

--- src/Controllers/BarterController.php
+++ src/Controllers/BarterController.php
@@ -2,11 +2,11 @@
 
 namespace MvcLite\Controllers;
 
-use MvcLite\Controllers\Engine\Controller;
+use MvcliteCore\Controllers\Controller;
 use MvcLite\Models\Offer;
-use MvcLite\Views\Engine\View;
+use MvcliteCore\Views\View;
 
-class BarterController extends Engine\Controller
+class BarterController extends Controller
 {
 
 
