f1b9b658820abe95e2aaeb86c779a865b76c44b2
quentinformatique <quentin161204@gmail.com>
2024-02-26 20:20:36 +0100

    add method to get user from code-agent

--- src/Models/User.php
+++ src/Models/User.php
@@ -2,16 +2,29 @@
 
 namespace MvcLite\Models;
 
+use MvcLite\Database\Engine\Database;
 use MvcLite\Models\Engine\Model;
 
 class User extends Model
 {
 
 
+    public static function newAgent(mixed $agentCode, mixed $phone, mixed $email): \MvcLite\Database\Engine\DatabaseQuery
+    {
+        return Database::query("INSERT INTO users (code_agent, phone, mail) VALUES (?, ?, ?)", [$agentCode, $phone, $email]);
+    }
+
+    public static function getUserByCode(mixed $agentCode): Engine\ModelCollection
+    {
+        return User::select()
+            ->where('code_agent', '=', $agentCode)
+            ->execute();
+    }
+
     public function getUserById(int $publisherId): Engine\ModelCollection
     {
 
-        return Model::select('users')
+        return User::select('users')
             ->where('id', '=', $publisherId)
             ->execute();
     }
