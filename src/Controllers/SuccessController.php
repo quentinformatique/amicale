c37ea35499ff48161ecdcc0dc9488c3cf73f9fe4
quentinformatique <quentin161204@gmail.com>
2024-03-03 22:48:39 +0100

    Create SuccessController.php

---
+++ src/Controllers/SuccessController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace MvcLite\Controllers;
+
+use MvcliteCore\Controllers\Controller;
+use MvcliteCore\Engine\InternalResources\Delivery;
+use MvcliteCore\Router\Request;
+use MvcliteCore\Views\View;
+
+class SuccessController extends Controller
+{
+    public function __construct() 
+    {
+        parent::__construct();
+    }
+    
+    public function render(): void
+    {
+        View::render("Success", [
+            "offer" => Delivery::get()->getProps()["offer"],
+        ]);
+    }
+}
